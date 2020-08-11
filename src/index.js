4f0e7c2e869fe77aeaea531f3e41dfd25c071198
roman9891 <roman9891@gmail.com>
2020-08-11 10:24:15 -0400

    up to edit button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+const url = `http://localhost:3000/dogs`
 document.addEventListener('DOMContentLoaded', () => {
+    const tableBody = document.querySelector(`#table-body`)
+    const form = document.querySelector(`form`)
+    const renderDogObject = (dog) => {
+            const tableRow = document.createElement(`tr`)
+            
+            tableRow.innerHTML = `
+                <td>${dog.name}</td>
+                <td>${dog.breed}</td>
+                <td>${dog.sex}</td>
+                <td><button class="edit-btn">Edit</button></td>
+            `
+            tableBody.append(tableRow)
+            
+        }
 
+    fetch(url)
+    .then(r => r.json())
+    .then(dogArray => dogArray.forEach(dogObject => renderDogObject(dogObject)))
+    
+    document.addEventListener(`click`, e => {
+        if (e.target.matches(`.edit-btn`)) {
+            const targetDog = e.target.parentNode.parentNode
+            form.name.value = targetDog.children[0].innerText
+            form.breed.value = targetDog.children[1].innerText
+            form.sex.value = targetDog.children[2].innerText
+            console.log(targetDog.children[0].innerText)
+        }
+    })
 })
