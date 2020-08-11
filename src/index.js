4dfa634b6cda29b0752d136d37980f1d4209ca98
sonyavera <sonyavgould@gmail.com>
2020-08-11 11:14:58 -0400

    Working solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-const dogsUrl = 'http://localhost:3000/dogs/'
+let dogsUrl = 'http://localhost:3000/dogs/'
 let dogsData = {}
 const tableBody = document.getElementById("table-body")
 const dogForm = document.getElementById("dog-form")
@@ -41,8 +41,45 @@ const grabDog = (e) => {
 }
 
 const populateEditForm = (dogObj) => {
-    
+    dogForm[0].value = `${dogObj.name}`
+    dogForm[1].value = `${dogObj.breed}`
+    dogForm[2].value = `${dogObj.sex}`
+
+    dogForm.addEventListener("submit", (e)=>{editDog(e, dogObj.id)})
 }
+
+const editDog = (e, dogId) => {
+    e.preventDefault()
+    const dogIndex = parseInt(dogId) - 1
+    let editedDog = {}
+    editedDog.name = e.target[0].value
+    editedDog.breed = e.target[1].value
+    editedDog.sex = e.target[2].value
+    dogsData[dogIndex] = editedDog
+
+    const options = {
+        method: "PATCH",
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify(editedDog)
+    }
+        
+    fetch(dogsUrl + dogId, options)
+    .then(response => response.json())
+    .then(resp => renderDogEdits(resp))
+}
+
+
+const renderDogEdits = (dogObj) => {
+    const dogDataRow = document.querySelector(`[data-dog-id='${dogObj.id}']`)
+    console.log(dogDataRow.children)
+    dogDataRow.children[0].innerText = dogObj.name
+    dogDataRow.children[1].innerText = dogObj.breed
+    dogDataRow.children[2].innerText = dogObj.sex
+}
+
 
 
 
