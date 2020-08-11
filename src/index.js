70e709cd5a9e15f5bcf080a958e6c49e3473375e
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-08-11 10:01:59 -0400

    Git dogs posted to table

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
+// DONE - render dogs to tr <td>
+// make dog editable
+//be able to update dog
+//render dogs pessimistically
+const url = "http://localhost:3000/dogs/"
+
 document.addEventListener('DOMContentLoaded', () => {
+
+    const getDogList = () => {
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => loadDogData(data))
+    }
+
+    const loadDogData = (data) => {
+        //iterate through data to grab dog objects
+
+        const dogBookShelf = document.getElementById("table-body")
+
+
+        data.forEach(dog => {
+
+            const dogCard = document.createElement('tr')
+            const dogName = dog.name
+            const dogBreed = dog.breed
+            const dogSex = dog.sex
+
+            dogCard.innerHTML = `
+                <td>${dogName}</td> <td>${dogBreed}</td> <td>${dogSex}</td> <td><button>Edit</button></td>
+            `
+
+            dogBookShelf.append(dogCard)
+
+        })
+
+
+
+    }
+
+
+    getDogList()
 
 })
