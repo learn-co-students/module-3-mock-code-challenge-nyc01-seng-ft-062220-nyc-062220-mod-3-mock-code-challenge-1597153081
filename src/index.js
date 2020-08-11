c06db6da06e3e0523f43e24f93dc43c420409469
roman9891 <roman9891@gmail.com>
2020-08-11 10:58:14 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const renderDogObject = (dog) => {
             const tableRow = document.createElement(`tr`)
             
+            tableRow.dataset.id = dog.id
             tableRow.innerHTML = `
                 <td>${dog.name}</td>
                 <td>${dog.breed}</td>
@@ -13,11 +14,18 @@ document.addEventListener('DOMContentLoaded', () => {
             `
             tableBody.append(tableRow)
             
-        }
+    }
+    const renderDogs = () => {
+        fetch(url)
+        .then(r => r.json())
+        .then(dogArray => dogArray.forEach(dogObject => renderDogObject(dogObject)))
+    }
+    const refreshDogs = () => {
+        tableBody.querySelectorAll(`tr`).forEach(tableRow => tableRow.remove())
+        renderDogs()
+    }
 
-    fetch(url)
-    .then(r => r.json())
-    .then(dogArray => dogArray.forEach(dogObject => renderDogObject(dogObject)))
+    renderDogs()
     
     document.addEventListener(`click`, e => {
         if (e.target.matches(`.edit-btn`)) {
@@ -25,7 +33,29 @@ document.addEventListener('DOMContentLoaded', () => {
             form.name.value = targetDog.children[0].innerText
             form.breed.value = targetDog.children[1].innerText
             form.sex.value = targetDog.children[2].innerText
-            console.log(targetDog.children[0].innerText)
+            form.dataset.currentId = targetDog.dataset.id
         }
+    })
+
+    document.addEventListener(`submit`, e => {
+        e.preventDefault()
+        const id = form.dataset.currentId
+        const dogObject = {
+            id: id,
+            name: form.name.value,
+            breed: form.breed.value,
+            sex: form.sex.value
+        }
+
+        fetch(url + `/${id}`, {
+            method: `PATCH`,
+            headers: {
+                "content-type": `application/json`,
+                accept: `application/json`
+            },
+            body: JSON.stringify(dogObject)
+        })
+        .then(r => r.json())
+        .then(() => refreshDogs())
     })
 })
