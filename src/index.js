44bf80eb51c4b8755dddd6d6796dbb00710f07ca
kylejb <47502769+kylejb@users.noreply.github.com>
2020-08-11 10:00:20 -0400

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 })
+
+/* 
+
+1-render list of dogs
+
+    >> TABLE STRUCTURE
+        NAME || BREED || SEX || EDIT DOG BUTTON
+    >> HTML
+        <tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>
+
+2-enable that edit button
+*   >> "Drop Down" Form
+    >> eventListener
+    >> PATCH 
+    >> Update DOM for "one page experience"
+
+
+*/
