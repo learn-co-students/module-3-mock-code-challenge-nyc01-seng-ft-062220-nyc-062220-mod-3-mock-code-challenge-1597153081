b36aba52f8dae1aa9a507db76d949b6c02c1951c
elandau91 <eli.b.landau@gmail.com>
2020-08-11 10:03:06 -0400

    deliverable 1 & 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+const URL = "http://localhost:3000/dogs/"
 
+    function renderDogs() {
+        fetch(URL)
+        .then(res => res.json())
+        .then(dogs => dogs.forEach(dog => addDog(dog)))
+    }
+
+    function addDog(dog) {
+        const table = document.querySelector("tbody")
+        const dogRow = document.createElement("tr")
+
+        dogRow.dataset.id = dog.id
+        dogRow.classList.add("dog")
+
+        dogRow.innerHTML = `
+        <td>${dog.name}</td> 
+        <td>${dog.breed}</td> 
+        <td>${dog.sex}</td> 
+        <td><button>Edit</button></td>
+        `
+
+
+        table.append(dogRow)
+    }
+
+
+renderDogs()
 })
