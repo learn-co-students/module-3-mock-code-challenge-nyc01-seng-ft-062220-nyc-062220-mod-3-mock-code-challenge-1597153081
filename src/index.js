5c3cdf37161e81cf78181adb3c868a50a8687039
hahm-d <dhahm01@gmail.com>
2020-08-11 11:42:23 -0400

    added more comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //declare global variables
     const URL = 'http://localhost:3000/dogs'
     const formParent = document.getElementById("dog-form").parentElement
     const tableBody = document.querySelector("tbody")
     const editForm = formParent.querySelector('#dog-form')
+    //create empty array, allDogs will be added during function fetchDogs
     let allDogs = []
 
     //api call to grab all dogs
@@ -10,7 +12,9 @@ document.addEventListener('DOMContentLoaded', () => {
         const res = await fetch(URL)
         const data = await res.json()
         data.forEach(dog => {
+            //for each dog in data add objs to allDogs
             allDogs.push(dog)
+            //render each dog to the tableBody
             renderDog(dog)
         })
     }
@@ -29,8 +33,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 
-    //redner each dog onto table 
+    //render each dog onto table
     const renderDog = (dog) => {
+        //create a new table row
         const tr = document.createElement("tr")
         tr.class = "padding"
         tr.innerHTML = `
@@ -39,22 +44,27 @@ document.addEventListener('DOMContentLoaded', () => {
               <th class="padding center">${dog.sex}</th>
               <th class="padding center"><button type="button" id=${dog.id}>Edit Dog</button></th>
               `
+        //add the new created table row onto the tableBody
         tableBody.append(tr)
     }
 
     //event on-click edit Dog
     const editHandler = () => {
         tableBody.addEventListener('click', e => {
+            //listen for click on button ONLY for tableBody
             if(e.target.matches("button")){
+                //e.target will always be button thus set that button id to dogId
                 const dogId = e.target.id
                 //find the dog obj from list
                 const currDog = allDogs.filter(dog => dog.id == dogId)
-
                 //update edit button form view
+                //taking advantage of children nodes! editForm is the partner that has all the values we need
                 editForm.children[0].value = currDog[0].name
                 editForm.children[1].value = currDog[0].breed
                 editForm.children[2].value = currDog[0].sex
                 editForm.children[3].id = currDog[0].id
+
+                //after this point- on edit button click, the form should be pre-populated.
             }
         })
     }
@@ -62,9 +72,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //forms submit invoked for above
     const formHandler = () => {
-        //eventListener
+        //eventListener on editForm since, we only care about clicks on the submit form
         editForm.addEventListener('submit', e => {
             e.preventDefault()
+            //using editForm grab each children's value
             let dogName = editForm.children[0].value
             let dogBreed = editForm.children[1].value
             let dogSex = editForm.children[2].value
@@ -75,15 +86,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 "breed": dogBreed,
                 "sex": dogSex
                 }
-                
+
             //invoke patch update 
+            //extra condition if the dog name is empty
             !dogObj.name ? alert('Invalid Entry!') : editDog(dogId, dogObj)
 
         })
 
     }
 
-    //invoke
+    //invoke functions!
     fetchDogs()
     editHandler()
     formHandler()
