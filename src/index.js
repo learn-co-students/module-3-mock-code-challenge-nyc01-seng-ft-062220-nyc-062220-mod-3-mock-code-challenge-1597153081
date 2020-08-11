542522dde49ed81e972a1f066349c28dda034912
kylejb <47502769+kylejb@users.noreply.github.com>
2020-08-11 10:27:28 -0400

    Rendered Registered Dogs from db.json

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const getDogs = async () => {
         let response = await fetch(PREFIX_URL);
         let result = await response.json();
-        console.log(result);
+        parseAndRenderDogs(result);
     }
 
     // params as placeholders for MVP design
@@ -28,6 +28,28 @@ document.addEventListener('DOMContentLoaded', () => {
         // google error handling for pessimistic approach if time permits
     }
 
+    const parseAndRenderDogs = (dogs) => {
+        const frag = new DocumentFragment();
+        const tableBody = document.getElementById("table-body")
+
+        for (const dog of dogs) {
+            const row = document.createElement("tr");
+            row.innerHTML = renderDogToRow(dog)
+            frag.appendChild(row)
+        }
+        tableBody.appendChild(frag);
+    }
+
+    const renderDogToRow = (dog) => {
+        return `<td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button data-dog-id="${dog.id}">Edit</button></td>`
+
+        // const row = document.createElement("tr");
+        
+        // return row.innerHTML = `<td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button data-dog-id="${dog.id}">Edit</button></td>`
+    }
+
+
+
 getDogs();
 
 })
@@ -36,12 +58,12 @@ getDogs();
 
 Notes for timed practice coding assessment at Flatiron School
 
-1-render list of dogs
+// 1-render list of dogs
 
-    >> TABLE STRUCTURE
-        NAME || BREED || SEX || EDIT DOG BUTTON
-    >> HTML
-        <tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>
+//     >> TABLE STRUCTURE
+//         NAME || BREED || SEX || EDIT DOG BUTTON
+//     >> HTML
+//         <tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>
 
 2-enable that edit button
 *   >> "Drop Down" Form
